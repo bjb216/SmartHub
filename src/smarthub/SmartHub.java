ecc8fa4c571996117264b3654c4efe8d9ee189bd
bartonb <bartonb@dyn141180.cc.lehigh.edu>
2016-01-27 15:22:20 -0500

    1 27

---
+++ src/smarthub/SmartHub.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package smarthub;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author bartonb
+ */
+public class SmartHub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a=5;
+         int b=5;
+         int c=a+b;
+         //System.out.println(c);
+         //System.out.println("hi");
+         System.out.println("January 27utput");
+        Scanner scan=new Scanner(System.in);
+        System.out.println("enter X: ");
+        int d=scan.nextInt();
+        
+        System.out.println("enter Y: ");
+        int e=scan.nextInt();
+        double test= d+e;
+        System.out.println("c+d: "+test);
+        // TODO code application logic here
+    }
+    
+}
