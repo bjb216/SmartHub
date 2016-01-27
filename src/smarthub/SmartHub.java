c865d499a0f5210086bf8b436501fe6c0054bb3b
bartonb <bartonb@dyn141180.cc.lehigh.edu>
2016-01-27 15:45:11 -0500

    commit v2

--- src/smarthub/SmartHub.java
+++ src/smarthub/SmartHub.java
@@ -1,16 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package smarthub;
 
 import java.util.Scanner;
 
-/**
- *
- * @author bartonb
- */
 public class SmartHub {
 
     /**
@@ -18,10 +10,6 @@ public class SmartHub {
      */
     public static void main(String[] args) {
         
-        //this is new
-        //
-        //
-        //
         int unused =7;
         int a=5;
          int b=5;
