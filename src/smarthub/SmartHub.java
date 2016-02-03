56c93c146c7730536af9ac0c9aa1799dc919660d
bartonb <bartonb@dyn141180.cc.lehigh.edu>
2016-02-03 15:45:11 -0500

    new line

--- src/smarthub/SmartHub.java
+++ src/smarthub/SmartHub.java
@@ -11,6 +11,7 @@ public class SmartHub {
     public static void main(String[] args) {
         
         System.out.println("welcome to smarthub");
+        System.out.println("new line");
     }
     
 }
