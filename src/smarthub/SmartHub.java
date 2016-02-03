0709bf58ed57430e4f3a60e68ff28814f40aba79
bartonb <bartonb@dyn141180.cc.lehigh.edu>
2016-02-03 15:24:33 -0500

    feb 3 test 2

--- src/smarthub/SmartHub.java
+++ src/smarthub/SmartHub.java
@@ -9,19 +9,7 @@ public class SmartHub {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-        int matt=10;
-        int brandon=1;
-         System.out.println("January 27utput");
-        Scanner scan=new Scanner(System.in);
-        System.out.println("enter X: ");
-        int d=scan.nextInt();
-        
-        System.out.println("enter Y: ");
-        int e=scan.nextInt();
-        double test= d+e;
-        System.out.println("c+d: "+test);
-        // TODO code application logic here
+        System.out.println("Welcome to SmartHub.");
     }
     
 }
