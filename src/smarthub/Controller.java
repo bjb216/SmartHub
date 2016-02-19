4c026cc9f898b3f62e26d29fb301fd71e1856f77
bartonb <bartonb@dyn141180.cc.lehigh.edu>
2016-02-19 15:31:28 -0500

    bluetooth communication completed, scanning correcting for android

--- src/smarthub/Controller.java
+++ src/smarthub/Controller.java
@@ -1,42 +1,90 @@
 package smarthub;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.bluetooth.BluetoothStateException;
 
 public class Controller {
+
     BTConnection con;
-    
-    
-    public Controller(){
+    ArrayList<User> users;
+    String[] menuItems = {"pair users", "current paired users", "Scan area", "exit"};
+    Scanner scan;
+
+    public Controller() {
         con = new BTConnection();
-        initialize();
+        //initialize();
+        scan = new Scanner(System.in);
+        users = new ArrayList<>();
     }
-    
-    private void initialize() {
-        try {
-            con.discover();
-        } catch (BluetoothStateException ex) {
-            Logger.getLogger(Controller.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (InterruptedException ex) {
-            Logger.getLogger(Controller.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-    public void run() {
-        if(con.devs!=null){
-            for(int i=0;i<con.devs.length;i++){
-                System.out.println("ID "+i+": "+con.devs[i]);
+
+    public void run() throws InterruptedException, IOException {
+
+        while (true) {
+            System.out.println("Welcome to SmartHub");
+            for (int i = 0; i < menuItems.length; i++) {
+                System.out.println((i + 1) + ": " + menuItems[i]);
+            }
+            int choice = scan.nextInt();
+            switch (choice) {
+                case 1:
+                    User user = con.pair();
+                    if (user != null) {
+                        users.add(user);
+                    }
+                    break;
+                case 2:
+                    printCurrentUsers();
+                    break;
+                case 3:
+                    scan();
+                    break;
+                case 4:
+                    System.out.println("goodbye");
+                    System.exit(0);
             }
         }
-        GCalendar cal = new GCalendar("xxxx","yyy");
-        ArrayList<String> events = cal.getMeetings();
-        
-        //Print out events
-        //for...
-        
-        
+
     }
-    
-    
+
+    private void scan(){
+        while(true){
+            for(int i=0;i<users.size();i++){
+                if(con.connect(users.get(i))){
+                    System.out.println(users.get(i).name+" in range");
+                }
+                else{
+                    System.out.println(users.get(i).name+" not in range");
+                }
+                    
+            }
+            delay(2);
+        }
+    }
+    private void delay(int time) {
+        try {
+            TimeUnit.SECONDS.sleep(time);
+        } catch (InterruptedException e) {
+            //Handle exception
+        }
+    }
+
+    private void printCurrentUsers() {
+        if (users.isEmpty()) {
+            System.out.println("No current users paired");
+        }
+        System.out.println("Current paired users");
+        for (int i = 0; i < users.size(); i++) {
+            System.out.println("Device: " + users.get(i).device);
+            System.out.println("Address: " + users.get(i).address);
+            System.out.println("Name: " + users.get(i).name);
+            System.out.println("\n");
+        }
+
+    }
+
 }
