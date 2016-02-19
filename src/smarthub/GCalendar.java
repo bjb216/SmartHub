8c8abd2a50178cf3109b128f1f4fffcb607e2290
bartonb <bartonb@dyn141180.cc.lehigh.edu>
2016-02-19 15:32:04 -0500

    bluetooth communication completed, scanning correcting for android

--- src/smarthub/GCalendar.java
+++ src/smarthub/GCalendar.java
@@ -1,22 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package smarthub;
 
 import java.util.ArrayList;
 
-/**
- *
- * @author bartonb
- */
 public class GCalendar {
     private ArrayList<String> meetings;
     private String username;
     private String password;
     
-    public GCalendar(String username, String password){
+    public GCalendar(String username, String password, String name){
         this.username=username;
         this.password=password;
         doStuff();
