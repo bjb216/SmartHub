bd079072dea99148fad31f4cb3e09c93b629d941
bartonb <bartonb@dyn141180.cc.lehigh.edu>
2016-02-29 15:49:21 -0500

    Voice somewhat implemented. Calendar authentication and event list working properly.

--- src/smarthub/User.java
+++ src/smarthub/User.java
@@ -1,5 +1,6 @@
 package smarthub;
 
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import javax.bluetooth.RemoteDevice;
 
 public class User {
@@ -7,6 +8,10 @@ public class User {
     public RemoteDevice device;
     public String address;
     public String name;
+    public String[] stocks={"AAPL","GOOG","VMW","INTC"};
+    
+    //CHECK TOKEN REFRESH ISSUES
+    public GoogleCredential credential;
     
     
     
@@ -14,6 +19,16 @@ public class User {
         this.device=device;
         this.address=address;
         this.name=name;
+        this.credential=null;
+    }
+    
+    //For testing purposes
+    public User(String name){
+        this.name=name;
+    }
+    
+    public void setCredential(GoogleCredential credential){
+        this.credential=credential;
     }
 
 }
