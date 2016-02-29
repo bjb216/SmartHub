e174044148205bc8ac83207d4cf48c5359f4e75e
bartonb <bartonb@dyn141180.cc.lehigh.edu>
2016-02-29 15:48:47 -0500

    Voice somewhat implemented. Calendar authentication and event list working properly.

--- src/smarthub/SmartHub.java
+++ src/smarthub/SmartHub.java
@@ -1,35 +1,12 @@
 package smarthub;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
-import java.util.Vector;
-import java.util.concurrent.TimeUnit;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.bluetooth.BluetoothStateException;
-import javax.bluetooth.DeviceClass;
-import javax.bluetooth.DiscoveryAgent;
-import static javax.bluetooth.DiscoveryAgent.PREKNOWN;
-import javax.bluetooth.DiscoveryListener;
-import javax.bluetooth.LocalDevice;
-import javax.bluetooth.RemoteDevice;
-import javax.bluetooth.ServiceRecord;
-import javax.microedition.io.Connector;
-import javax.obex.ClientSession;
 import org.json.JSONException;
 
 public class SmartHub {
-    public static final String bjbid = "703EAC1A1042";
-    public static final String mdbid = "10D542EFEC45";
-
     public static void main(String[] args) throws BluetoothStateException, InterruptedException, IOException, JSONException {
         Controller con = new Controller();
         con.run();
-       
     }
-
 }
