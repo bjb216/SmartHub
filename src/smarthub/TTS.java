cb5f3c0b713d00cb4def7156cada1e523f590be0
bartonb <bartonb@dyn141180.cc.lehigh.edu>
2016-03-29 19:40:38 -0400

    commit on march 29

---
+++ src/smarthub/TTS.java
@@ -0,0 +1,82 @@
+package smarthub;
+
+import com.amazonaws.auth.ClasspathPropertiesFileCredentialsProvider;
+import com.ivona.services.tts.IvonaSpeechCloudClient;
+import com.ivona.services.tts.model.CreateSpeechRequest;
+import com.ivona.services.tts.model.CreateSpeechResult;
+import com.ivona.services.tts.model.Input;
+import com.ivona.services.tts.model.Voice;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class TTS {
+
+    private static String secretKey = "2IqTnmUkdlama7WkTUdYODCuYuIKqxb2BmcQPdcI";
+    private static String accessKey = "GDNAJA5USEBJNWSMGXFA";
+
+    private static IvonaSpeechCloudClient speechCloud;
+
+    private static void init() {
+        speechCloud = new IvonaSpeechCloudClient(new IvonaCredentials(secretKey, accessKey));
+        //speechCloud = new IvonaSpeechCloudClient(new IvonaCredentials("secretKey", "accessKey"));
+       
+        //speechCloud = new IvonaSpeechCloudClient(new ClasspathPropertiesFileCredentialsProvider("resources/IvonaCredentials.properties"));
+        speechCloud.setEndpoint("https://tts.eu-west-1.ivonacloud.com");
+    }
+
+    public void doSomething() throws IOException {
+        init();
+
+        String outputFileName = "/tmp/speech.mp3";
+        CreateSpeechRequest createSpeechRequest = new CreateSpeechRequest();
+        Input input = new Input();
+        Voice voice = new Voice();
+
+        voice.setName("Salli");
+        input.setData("This is a sample text to be synthesized.");
+
+        createSpeechRequest.setInput(input);
+        createSpeechRequest.setVoice(voice);
+        InputStream in = null;
+        FileOutputStream outputStream = null;
+
+        try {
+
+            CreateSpeechResult createSpeechResult = speechCloud.createSpeech(createSpeechRequest);
+
+            System.out.println("\nSuccess sending request:");
+            System.out.println(" content type:\t" + createSpeechResult.getContentType());
+            System.out.println(" request id:\t" + createSpeechResult.getTtsRequestId());
+            System.out.println(" request chars:\t" + createSpeechResult.getTtsRequestCharacters());
+            System.out.println(" request units:\t" + createSpeechResult.getTtsRequestUnits());
+
+            System.out.println("\nStarting to retrieve audio stream:");
+
+            in = createSpeechResult.getBody();
+            outputStream = new FileOutputStream(new File(outputFileName));
+
+            byte[] buffer = new byte[2 * 1024];
+            int readBytes;
+
+            while ((readBytes = in.read(buffer)) > 0) {
+                // In the example we are only printing the bytes counter,
+                // In real-life scenario we would operate on the buffer
+                System.out.println(" received bytes: " + readBytes);
+                outputStream.write(buffer, 0, readBytes);
+            }
+
+            System.out.println("\nFile saved: " + outputFileName);
+
+        } finally {
+            if (in != null) {
+                in.close();
+            }
+            if (outputStream != null) {
+                outputStream.close();
+            }
+        }
+    }
+
+}
