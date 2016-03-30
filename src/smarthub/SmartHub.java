cfe811fb474ce4149eca29517fd9c06cfd4a9eee
bartonb <bartonb@dyn141180.cc.lehigh.edu>
2016-03-29 20:44:21 -0400

    New weather shit and speech working deployed to mp3 file

--- src/smarthub/SmartHub.java
+++ src/smarthub/SmartHub.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import javax.bluetooth.BluetoothStateException;
 import org.json.JSONException;
 
+
+
 public class SmartHub {
     public static void main(String[] args) throws BluetoothStateException, InterruptedException, IOException, JSONException {
         Controller con = new Controller();
