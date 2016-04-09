730ef8b4b131f0b622794c38e27ef9c31b454521
bartonb <bartonb@dyn141180.cc.lehigh.edu>
2016-04-09 15:27:56 -0400

    Commit 4/9/16 working GUI from poster demo

--- src/smarthub/SmartHub.java
+++ src/smarthub/SmartHub.java
@@ -4,8 +4,6 @@ import java.io.IOException;
 import javax.bluetooth.BluetoothStateException;
 import org.json.JSONException;
 
-
-
 public class SmartHub {
     public static void main(String[] args) throws BluetoothStateException, InterruptedException, IOException, JSONException {
         Controller con = new Controller();
