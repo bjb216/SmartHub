35aecbd3f0423ddf13a1a4634b2b481ba32b8e3d
bartonb <bartonb@dyn141180.cc.lehigh.edu>
2016-04-09 19:10:17 -0400

    updated gui with finance and user preferences working

--- src/smarthub/FinancialData.java
+++ src/smarthub/FinancialData.java
@@ -4,6 +4,8 @@ package smarthub;
 import java.io.IOException;
 import yahoofinance.Stock;
 import yahoofinance.YahooFinance;
+import java.util.ArrayList;
+import java.math.*;
 
 
 
@@ -19,12 +21,33 @@ public class FinancialData {
         this.stocks=user.stocks;
     }
     
+    public void populateStockArray(String[] stockList, ArrayList stockArray) throws IOException{
+        String ticker;
+        BigDecimal cost;
+        BigDecimal percent;
+        
+        for (int i = 0; i <stocks.length; i++){
+            Stock stk = YahooFinance.get(stocks[i]);
+            ticker = stocks[i];
+            cost = stk.getQuote().getPrice();
+            percent = stk.getQuote().getChangeInPercent();
+            
+            SingleStock tempStock = new SingleStock(ticker, cost, percent);
+            
+            stockArray.add(tempStock);
+        }
+    }
+    
     public void printInfo() throws IOException{
         for (int i=0;i<stocks.length;i++){
             Stock stk=YahooFinance.get(stocks[i]);
             System.out.println(stocks[i]+" "+stk.getQuote().getPrice()+" "+stk.getQuote().getChangeInPercent());
         }
     }
+    
+   /* public String getPrice(){
+        return stk.getQuote().getPrice();
+    }*/
     
     
     
