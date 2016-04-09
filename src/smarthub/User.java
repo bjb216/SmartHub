417480e659862c2eb7b48d8adddd3f054843423e
bartonb <bartonb@dyn141180.cc.lehigh.edu>
2016-04-09 19:10:11 -0400

    updated gui with finance and user preferences working

--- src/smarthub/User.java
+++ src/smarthub/User.java
@@ -12,6 +12,15 @@ public class User {
     public String name;
     public String[] stocks={"AAPL","GOOG","VMW","INTC"};
     
+    
+    //PREFERENCES
+    public boolean finSmall;
+    public boolean weatherSmall;
+    public boolean calSmall;
+    public boolean finTalk;
+    public boolean weatherTalk;
+    public boolean calTalk;
+    
     //CHECK TOKEN REFRESH ISSUES
     public GoogleCredential credential;
    
@@ -20,6 +29,13 @@ public class User {
         this.address=address;
         this.name=name;
         this.credential=null;
+        finSmall=true;
+        weatherSmall=true;
+        calSmall=true;
+        finTalk=true;
+        weatherTalk=true;
+        calTalk=true;
+        
     }
     
     //For testing purposes
@@ -29,6 +45,86 @@ public class User {
     
     public void setCredential(GoogleCredential credential){
         this.credential=credential;
+    }
+    
+    public String getState(String str){
+        switch (str) {
+            case "financial":
+                if(finSmall==true)
+                    return "Condensed";
+                else
+                    return "Extended";
+            case "weather":
+                if(weatherSmall==true)
+                    return "Condensed";
+                else
+                    return "Extended";
+            case "calendar":
+                if(calSmall==true)
+                    return "Condensed";
+                else
+                    return "Extended";
+            default:
+                break;
+        }
+        return null;
+    }
+    
+    public String getTalk(String str){
+        switch (str) {
+            case "financial":
+                if(finTalk==true)
+                    return "Audio";
+                else
+                    return "No Audio";
+            case "weather":
+                if(weatherTalk==true)
+                    return "Audio";
+                else
+                    return "No Audio";
+            case "calendar":
+                if(calTalk==true)
+                    return "Audio";
+                else
+                    return "No Audio";
+            default:
+                break;
+        }
+        return null;
+    }
+    
+    public void changeState(String str){
+        switch (str) {
+            case "financial":
+                finSmall = !finSmall;
+                break;
+            case "calendar":
+                calSmall = !calSmall;
+                break;
+            case "weather":
+                weatherSmall = !weatherSmall;
+                break;
+            default:
+                break;
+        }
+        
+    }
+    
+    public void changeTalk(String str){
+        switch (str) {
+            case "financial":
+                finTalk = !finTalk;
+                break;
+            case "calendar":
+                calTalk = !calTalk;
+                break;
+            case "weather":
+                weatherTalk = !weatherTalk;
+                break;
+            default:
+                break;
+        }
+        
     }
 
 }
