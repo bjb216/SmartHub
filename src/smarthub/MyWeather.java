f655b1cfc28193d898cfe92965c38e9bbc631ebd
bartonb <bartonb@dyn141180.cc.lehigh.edu>
2016-04-13 16:07:23 -0400

    Commit 4-13

--- src/smarthub/MyWeather.java
+++ src/smarthub/MyWeather.java
@@ -11,24 +11,19 @@ import net.aksingh.owmjapis.OpenWeatherMap;
 import org.json.JSONException;
 import java.util.Date;
 import java.util.ArrayList;
+import net.aksingh.owmjapis.HourlyForecast;
 
 public class MyWeather {
 
     public OpenWeatherMap owm;
     String city;
 
-    
-    
     //Constructer
     public MyWeather(String city) {
         owm = new OpenWeatherMap("457fbd57a1d3f89a7c4db388f84338bf");
         this.city = city;
     }
 
-    //Create a Day object
-    public void getHourly() {
-
-    }
 
     /*Create a day object that will have high temp, low temp, boolean precipitation, wind
       buld a day object, build a weekly array
@@ -36,7 +31,9 @@ public class MyWeather {
     public void multiDayForecast(ArrayList forecastArray, int dayNumber) throws IOException, JSONException {
         System.out.println("weekly");
         byte size = 0;
+
         DailyForecast daily = owm.dailyForecastByCityName(city, size);
+
         City c = daily.getCityInstance();
         int count = dayNumber;
 
@@ -83,10 +80,63 @@ public class MyWeather {
         //System.out.println("The Size of the Arraylist is: " + forecast.size());
     }
 
+    //Hourly Forecast
+    public void hourlyForecast(ArrayList forecastArray, int hourNumber) throws IOException, JSONException {
+        byte size = 0;
+        int index = 0;
+        DailyForecast daily = owm.dailyForecastByCityName(city, size);
+        HourlyForecast hourly = owm.hourlyForecastByCityName(city);
+        City c = daily.getCityInstance();
+        int count = hourNumber;
+
+        //Weather input variables
+        String weather;
+        String hourTime;
+
+        System.out.println(c.getCityName() + " " + c.getCountryCode());
+
+        /*System.out.println("***********HOURLY TEST************");
+        System.out.println(hourly.getForecastInstance(0).getMainInstance().getMaxTemperature());
+        System.out.println(hourly.getForecastInstance(0).getDateTimeText());
+        System.out.println(hourly.getForecastInstance(1).getDateTimeText());
+        System.out.println(hourly.getForecastInstance(2).getDateTimeText());
+        System.out.println(hourly.getForecastInstance(0).getMainInstance().getTemperature());
+        System.out.println(hourly.getForecastInstance(0).getWeatherInstance(0).getWeatherDescription());
+        System.out.println(hourly.getForecastInstance(0).getWeatherInstance(0).getWeatherName());
+        System.out.println("**********************************");
+         */
+        //Changed the i from beign 0 to 1
+        for (int i = 1; i < (hourNumber + 2); i++) {
+            Forecast ft = daily.getForecastInstance(i);//Having the problem here
+            Weather w = ft.getWeatherInstance(0);
+            Temperature t = ft.getTemperatureInstance();
+
+            hourTime = hourly.getForecastInstance(i).getDateTimeText();
+            weather = hourly.getForecastInstance(i).getWeatherInstance(0).getWeatherDescription();
+
+            HourlyWeather tempHour = new HourlyWeather(hourTime, weather);
+
+            forecastArray.add(tempHour);
+
+        }
+    }
+
+    public void printHourlyForecast(ArrayList<HourlyWeather> forecast) {
+        //Changed i to be a 1 instead of a 0
+        for (int i = 0; i < forecast.size(); i++) {
+            System.out.println("");
+            System.out.println("Hour Number: " + (i + 1));
+            forecast.get(i).printHourForecast();
+            System.out.println("");
+        }
+        //System.out.println("The Size of the Arraylist is: " + forecast.size());
+    }
+
     public void getDaily() throws IOException, JSONException {
         System.out.println("Daily");
         byte size = 0;
         DailyForecast daily = owm.dailyForecastByCityName(city, size);
+
         City c = daily.getCityInstance();
         System.out.println(c.getCityName() + " " + c.getCountryCode());
 
@@ -121,6 +171,7 @@ public class MyWeather {
         // getting current weather data for the "London" city
         //CurrentWeather cwd2 = owm.
         CurrentWeather cwd = owm.currentWeatherByCityName("Bethlehem");
+
         //cwd.
         //cwd.
 //printing city name from the retrieved data
