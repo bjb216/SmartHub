01e476cb559eb42bcb00c5ff102ade012b7b8656
bartonb <bartonb@dyn141180.cc.lehigh.edu>
2016-04-13 16:07:08 -0400

    Commit 4-13

--- src/smarthub/FinancialData.java
+++ src/smarthub/FinancialData.java
@@ -2,10 +2,14 @@
 package smarthub;
 
 import java.io.IOException;
+import static java.lang.Boolean.FALSE;
 import yahoofinance.Stock;
 import yahoofinance.YahooFinance;
 import java.util.ArrayList;
 import java.math.*;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import yahoofinance.quotes.stock.StockDividend;
 
 
 
@@ -23,16 +27,30 @@ public class FinancialData {
     
     public void populateStockArray(String[] stockList, ArrayList stockArray) throws IOException{
         String ticker;
+        String name;
         BigDecimal cost;
         BigDecimal percent;
+        BigDecimal divYield;
+        BigDecimal ytdChange;
+        YahooFinance.logger.setUseParentHandlers(false);
         
         for (int i = 0; i <stocks.length; i++){
             Stock stk = YahooFinance.get(stocks[i]);
             ticker = stocks[i];
             cost = stk.getQuote().getPrice();
             percent = stk.getQuote().getChangeInPercent();
+            name = stk.getName();
+            StockDividend d = stk.getDividend();
+            divYield = d.getAnnualYieldPercent();
+            Calendar c = new GregorianCalendar(2016,1,1);
+            System.out.println("num of data points"+stk.getHistory(c).size());
+            System.out.println("stock price jan 1: "+ stk.getHistory(c).get(2).getClose());
+            ytdChange = cost.divide(stk.getHistory(c).get(0).getClose(),2,BigDecimal.ROUND_HALF_UP);
+            System.out.println(name +": "+ytdChange);
+            BigDecimal one = new BigDecimal(1);
+            ytdChange = ytdChange.subtract(one);
             
-            SingleStock tempStock = new SingleStock(ticker, cost, percent);
+            SingleStock tempStock = new SingleStock(ticker, cost, percent, name, divYield,ytdChange);
             
             stockArray.add(tempStock);
         }
@@ -41,7 +59,7 @@ public class FinancialData {
     public void printInfo() throws IOException{
         for (int i=0;i<stocks.length;i++){
             Stock stk=YahooFinance.get(stocks[i]);
-            System.out.println(stocks[i]+" "+stk.getQuote().getPrice()+" "+stk.getQuote().getChangeInPercent());
+            //System.out.println(stocks[i]+" "+stk.getQuote().getPrice()+" "+stk.getQuote().getChangeInPercent());
         }
     }
     
