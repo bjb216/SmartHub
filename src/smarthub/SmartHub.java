838a220c7e130f5b57a64ad69a60322e16e99277
bartonb <bartonb@dyn141180.cc.lehigh.edu>
2016-04-13 16:07:34 -0400

    Commit 4-13

--- src/smarthub/SmartHub.java
+++ src/smarthub/SmartHub.java
@@ -1,11 +1,15 @@
 package smarthub;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
 import javax.bluetooth.BluetoothStateException;
 import org.json.JSONException;
 
 public class SmartHub {
-    public static void main(String[] args) throws BluetoothStateException, InterruptedException, IOException, JSONException {
+    public static void main(String[] args) throws BluetoothStateException, InterruptedException, IOException, JSONException, URISyntaxException {
+        
+        //BTConnection con = new BTConnection();
+        
         Controller con = new Controller();
         con.run();
     }
