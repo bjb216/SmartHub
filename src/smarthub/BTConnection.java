b14c3653f9c6c334cbf06817f2825193f48dad05
bartonb <bartonb@dyn141180.cc.lehigh.edu>
2016-04-21 10:48:05 -0400

    Bluetooth.Fucking.Works. TTS.Fucking.Works

--- src/smarthub/BTConnection.java
+++ src/smarthub/BTConnection.java
@@ -1,5 +1,6 @@
 package smarthub;
 
+import com.intel.bluetooth.btgoep.Connection;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
@@ -8,6 +9,8 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.Vector;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.bluetooth.BluetoothStateException;
 import javax.bluetooth.DataElement;
 import javax.bluetooth.DeviceClass;
@@ -44,34 +47,50 @@ public class BTConnection {
     }
 
     //Catch these errors
-    public User pair(JPanel user,JFrame frame) throws BluetoothStateException, InterruptedException, IOException {
-        System.out.println("pair called");
-        discoverDevices();
-        System.out.println("returned from discoverDevices");
-        if (devicesDiscovered == null || devicesDiscovered.isEmpty()) {
-            JOptionPane.showMessageDialog(frame, "No Devices in range");
-            return null;
-        }
-
-        for (int i = 0; i < devicesDiscovered.size(); i++) {
-            String message ="Would you like to pair device: "+ devicesDiscovered.get(i).getFriendlyName(true);
-            int reply = JOptionPane.showConfirmDialog(frame, message, null, JOptionPane.YES_NO_OPTION);
-
-            if (reply==JOptionPane.YES_OPTION) {
-                if (devices.contains(devicesDiscovered.get(i))) {
-                    JOptionPane.showMessageDialog(frame, "Device already Paired");
-                } else {
-                    discoverServices(devicesDiscovered.get(i));
-                    System.out.println("adding new user");
-                    System.out.println(urls.get(0));
-                    devices.add(devicesDiscovered.get(i));
-                    JOptionPane.showMessageDialog(frame, "Device added");
-                    return new User(devicesDiscovered.get(i), urls.get(0), devicesDiscovered.get(i).getFriendlyName(true));
-                }
-            } else {
-                JOptionPane.showMessageDialog(frame, "Device not added");
+    public User pair(JPanel user, JFrame frame) {
+        try {
+            System.out.println("pair called");
+            discoverDevices();
+            System.out.println("returned from discoverDevices");
+            if (devicesDiscovered == null || devicesDiscovered.isEmpty()) {
+                JOptionPane.showMessageDialog(frame, "No Devices in range");
+                return null;
             }
 
+            for (int i = 0; i < devicesDiscovered.size(); i++) {
+                System.out.println("in for loop");
+                String message = "Would you like to pair device: " + devicesDiscovered.get(i).getFriendlyName(true);
+                int reply = JOptionPane.showConfirmDialog(frame, message, null, JOptionPane.YES_NO_OPTION);
+
+                if (reply == JOptionPane.YES_OPTION) {
+                    if (devices.contains(devicesDiscovered.get(i))) {
+                        JOptionPane.showMessageDialog(frame, "Device already Paired");
+                    } else {
+
+                            discoverServices(devicesDiscovered.get(i));
+                            System.out.println("adding new user");
+                            if (urls.isEmpty()) {
+                                System.out.println("No urls found");
+                                JOptionPane.showMessageDialog(frame, "Error: Device not added");
+                                return null;
+                            }
+                            System.out.println("url found");
+                            devices.add(devicesDiscovered.get(i));
+                            JOptionPane.showMessageDialog(frame, "Device added");
+                            return new User(devicesDiscovered.get(i), urls.get(0), devicesDiscovered.get(i).getFriendlyName(true));
+                        } 
+                    
+                } else {
+                    JOptionPane.showMessageDialog(frame, "Device not added");
+                    //return null;
+                }
+
+            }
+
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(frame, "Error: Device not added");
+            System.out.println("error mesage: "+ex.getMessage());
+            return null;
         }
         return null;
     }
@@ -82,7 +101,7 @@ public class BTConnection {
 
         DiscoveryListener listener = new DiscoveryListener() {
             public void deviceDiscovered(RemoteDevice btDevice, DeviceClass cod) {
-                //System.out.println("Device " + btDevice.getBluetoothAddress() + " found");
+                System.out.println("Device " + btDevice.getBluetoothAddress() + " found");
                 //devicesDiscovered.addElement(btDevice);
                 devicesDiscovered.add(btDevice);
                 //devices.add(btDevice);
@@ -110,7 +129,7 @@ public class BTConnection {
             if (started) {
                 //System.out.println("wait for device inquiry to complete...");
                 inquiryCompletedEvent.wait();
-                //System.out.println(devicesDiscovered.size() + " device(s) found");
+                System.out.println(devicesDiscovered.size() + " device(s) found");
             }
         }
 
@@ -129,7 +148,14 @@ public class BTConnection {
         UUID[] serviceUUID = new UUID[1];
 
         //0x1105 works for Matt's phone!!!!!!
-        serviceUUID[0] = new UUID(0x1105);
+        //OBEX Object Push Profile
+        //serviceUUID[0] = new UUID(0x1105);
+        
+        //Personal area network user
+        //serviceUUID[0] = new UUID(0x1115);
+        
+        //Handsfree profile
+        serviceUUID[0] = new UUID(0x111E);
 
         final Object serviceSearchCompletedEvent = new Object();
 
@@ -151,9 +177,9 @@ public class BTConnection {
                     urls.add(url);
                     DataElement serviceName = servRecord[i].getAttributeValue(0x0100);
                     if (serviceName != null) {
-                        //System.out.println("service " + serviceName.getValue() + " found " + url);
+                        System.out.println("service " + serviceName.getValue() + " found " + url);
                     } else {
-                        //System.out.println("service found " + url);
+                        System.out.println("service found " + url);
                     }
                 }
             }
@@ -173,28 +199,59 @@ public class BTConnection {
         };
 
         synchronized (serviceSearchCompletedEvent) {
-            //System.out.println("search services on " + device.getBluetoothAddress() + " " + device.getFriendlyName(false));
+            System.out.println("search services on " + device.getBluetoothAddress() + " " + device.getFriendlyName(false));
             LocalDevice.getLocalDevice().getDiscoveryAgent().searchServices(null, searchUuidSet, device, listener);
             serviceSearchCompletedEvent.wait();
         }
 
     }
 
-    public boolean connect(User user) {
+    
+        public boolean connect(User user) {
+        try {
+            System.out.println("connecting to user: "+user.name);
+            Connector.open(user.address);
+            return true;
+
+            //Catching all exceptions. used to be an IO exception 
+        } catch (Exception ex) {
+            if(ex.getMessage().contains("Host is down")){
+                System.out.println("device not found");
+                return false;
+            }
+            else{
+                System.out.println(ex.getMessage()+":::but returning true");
+                return true;
+            }
+        }
+
+    }
+
+    
+    
+    public boolean connect2(User user) {
         ClientSession clientSession;
         HeaderSet hsConnectReply;
         try {
+            System.out.println("connecting to user: "+user.name);
             clientSession = (ClientSession) Connector.open(user.address);
             hsConnectReply = clientSession.connect(null);
+            clientSession.close();
+            return true;
+            
 
-            if (hsConnectReply.getResponseCode() != ResponseCodes.OBEX_HTTP_OK) {
-                return false;
-            } else {
-                clientSession.close();
-                return true;
-            }
+//            //old
+//            //hsConnectReply.getResponseCode() != ResponseCodes.OBEX_HTTP_OK
+//            
+//            if (hsConnectReply.getResponseCode() != ResponseCodes.OBEX_HTTP_OK) {
+//                return false;
+//            } else {
+//                clientSession.close();
+//                return true;
+//            }
             //Catching all exceptions. used to be an IO exception 
         } catch (Exception ex) {
+            System.out.println(ex.getMessage());
             return false;
         }
 
