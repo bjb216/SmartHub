7a868c3f7ffc58cdba00607c81c8a15ca97251fc
bartonb <bartonb@dyn141180.cc.lehigh.edu>
2016-04-22 14:08:01 -0400

    fri apr 22

--- src/smarthub/User.java
+++ src/smarthub/User.java
@@ -3,14 +3,19 @@ package smarthub;
 
 
 import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import java.util.ArrayList;
 import javax.bluetooth.RemoteDevice;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 public class User {
 
     public RemoteDevice device;
     public String address;
     public String name;
-    public String[] stocks={"AAPL","GOOG","VMW","INTC"};
+    //public String[] stocks={"AAPL","GOOG","VMW","INTC"};
+    public ArrayList<String> stocks;
+    public JFrame frame;
     
     
     //PREFERENCES
@@ -36,11 +41,35 @@ public class User {
         weatherTalk=true;
         calTalk=true;
         
+        stocks = new ArrayList<>();
+        stocks.add("AAPL");
+        stocks.add("VMW");
     }
     
     //For testing purposes
-    public User(String name){
+    public User(String name, JFrame frame){
+        this.frame=frame;
         this.name=name;
+        this.credential=null;
+        finSmall=true;
+        weatherSmall=true;
+        calSmall=true;
+        finTalk=true;
+        weatherTalk=true;
+        calTalk=true;
+        
+        stocks = new ArrayList<>();
+        stocks.add("AAPL");
+        stocks.add("VMW");
+    }
+    
+    public void addStock(String ticker){
+        if(FinancialData.isValid(ticker)){
+            JOptionPane.showMessageDialog(frame, ticker+" added.");
+        }
+        else{
+            JOptionPane.showMessageDialog(frame, ticker+" is an invalid ticker");
+        }
     }
     
     public void setCredential(GoogleCredential credential){
